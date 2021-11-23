526a042c897bb078cdbb8a78ef8ea407fdc0e74c
Vladimir <freegun.super@yandex.ru>
2021-11-24 00:38:47 +0500

    fix

--- js/lexer.js
+++ js/lexer.js
@@ -1,38 +1,84 @@
-function token(initType, initValue) {
-  this.type = initType;
-  this.value = initValue;
-
-  this.stringRep = function () {
-    return "TOKEN(" + this.type + ", " + this.value + ")";
-  }
+function token(type, value) {
+  this.type = type;
+  this.value = value;
 }
 
-function lexer(initText) {
-  this.text = initText;
+function lexer(text) {
+  this.text = text;
   this.position = 0;
   this.currentChar = this.text[this.position];
+
+  this.id_list = []
+  this.constants_list = []
+
+  this.separators_list = [
+    '+',
+    '-',
+    '*',
+    '/',
+    '.',
+    ',',
+    ':',
+    ';',
+    '=',
+    '<',
+    '<=',
+    '>',
+    '>=',
+    ':=',
+    '(',
+    ')',
+    '[',
+    ']'
+  ]
+
+  this.reservedKeywords_list = [
+    'BEGIN',
+    'END',
+    'PROGRAM',
+    'VAR',
+    'INTEGER',
+    'REAL',
+    'ELSE',
+    'IF',
+    'THEN',
+    'DIV',
+    'REPEAT',
+    'CASE',
+    'SWITCH',
+    'ARRAY',
+    'WHILE',
+    'DO',
+    'UNTIL',
+    'OF',
+    'PROCEDURE',
+    'OR'
+  ]
 
   this.reservedKeywords = {
     BEGIN: new token("BEGIN", "BEGIN"),
     END: new token("END", "END"),
-    PROGRAM: new token("PROGRAM", "PROGRAM")
+    PROGRAM: new token("PROGRAM", "PROGRAM"),
+    VAR: new token("VAR", "VAR"),
+    INTEGER: new token("INTEGER", "INTEGER"),
+    REAL: new token("REAL", "REAL"),
+    ELSE: new token("ELSE", "ELSE"),
+    IF: new token("IF", "IF"),
+    THEN: new token("THEN", "THEN"),
+    DIV: new token("DIV", "DIV"),
+    REPEAT: new token("REPEAT", "REPEAT"),
+    CASE: new token("CASE", "CASE"),
+    SWITCH: new token("SWITCH", "SWITCH"),
+    ARRAY: new token("ARRAY", "ARRAY"),
+    WHILE: new token("WHILE", "WHILE"),
+    DO: new token("DO", "DO"),
+    UNTIL: new token("UNTIL", "UNTIL"),
+    OF: new token("OF", "OF"),
+    PROCEDURE: new token("PROCEDURE", "PROCEDURE"),
+    OR: new token("OR", "OR"),
   }
 
-  this.id = function () {
-    var result = "";
-    while (this.currentChar != null && /[a-z]/i.test(this.currentChar)) {
-      result += this.currentChar;
-      this.advance();
-    }
-    if (result in this.reservedKeywords)
-      return this.reservedKeywords[result];
-    else
-      return new token("ID", result);
-  }
 
-  this.error = function () {
-    throw "Invalid character"
-  }
 
   this.advance = function () {
     this.position++;
@@ -42,22 +88,40 @@ function lexer(initText) {
       this.currentChar = this.text[this.position];
   }
 
-  this.skipWhiteSpace = function () {
+  this.id = function () {
+    let result = "";
+    while (this.currentChar != null && /[a-z]/i.test(this.currentChar)) {
+      result += this.currentChar;
+      this.advance();
+    }
+    if (result in this.reservedKeywords)
+      return this.reservedKeywords[result];
+    else {
+      if (!this.id_list.includes(result)) this.id_list.push(result);
+      return new token("ID", result);
+    }
+  }
+
+  this.error = function () {
+    throw `Неизвестный символ на позиции: ${this.position} ("${this.currentChar}")`
+  }
+
+  this.skipSpace = function () {
     while (this.currentChar != null && this.currentChar == " ")
       this.advance();
   }
 
-  this.integer = function () {
-    var result = "";
+  this.number = function () {
+    let result = "";
     while (this.currentChar != null && !isNaN(this.currentChar)) {
       result += this.currentChar;
       this.advance();
     }
-    return parseInt(result);
+    return +result;
   }
 
   this.peek = function () {
-    var peekPos = this.position + 1;
+    let peekPos = this.position + 1;
     if (peekPos > this.text.length - 1)
       return null;
     else
@@ -69,10 +133,34 @@ function lexer(initText) {
     while (this.currentChar != null) {
 
       if (this.currentChar == " ")
-        this.skipWhiteSpace();
+        this.skipSpace();
 
-      if (!isNaN(this.currentChar))
-        return new token("INTEGER", this.integer());
+      if (!isNaN(this.currentChar)) {
+        const number = this.number();
+        if (!this.constants_list.includes(number)) this.constants_list.push(number);
+        return new token("NUMBER", number);
+      }
+
+      if (/[a-z]/i.test(this.currentChar))
+        return this.id();
+
+      if (this.currentChar == ":" && this.peek() == "=") {
+        this.advance();
+        this.advance();
+        return new token("ASSIGN", ":=");
+      }
+
+      if (this.currentChar == "<" && this.peek() == "=") {
+        this.advance();
+        this.advance();
+        return new token("LE", "<=");
+      }
+
+      if (this.currentChar == ">" && this.peek() == "=") {
+        this.advance();
+        this.advance();
+        return new token("GE", ">=");
+      }
 
       if (this.currentChar == "+") {
         this.advance();
@@ -104,13 +192,24 @@ function lexer(initText) {
         return new token("RPAREN", ")");
       }
 
-      if (/[a-z]/i.test(this.currentChar))
-        return this.id();
+      if (this.currentChar == "=") {
+        this.advance();
+        return new token("EQ", "=");
+      }
 
-      if (this.currentChar == ":" && this.peek() == "=") {
+      if (this.currentChar == "<") {
         this.advance();
+        return new token("LW", "<");
+      }
+
+      if (this.currentChar == ">") {
         this.advance();
-        return new token("ASSIGN", ":=");
+        return new token("GR", ">");
+      }
+
+      if (this.currentChar == ":") {
+        this.advance();
+        return new token("COLON", ":");
       }
 
       if (this.currentChar == ";") {
@@ -128,13 +227,33 @@ function lexer(initText) {
         return new token("SEP", ",");
       }
 
+      if (this.currentChar == "[") {
+        this.advance();
+        return new token("SLPAREN", "[");
+      }
+
+      if (this.currentChar == "]") {
+        this.advance();
+        return new token("SRPAREN", "]");
+      }
+
       this.error();
     }
     return new token("EOF", null);
   }
 }
 
-const program_text = "PROGRAM PRIMER; VAR x, y, z"
+const program_text = "PROGRAM spo; \
+VAR a, b, c: INTEGER; \
+BEGIN \
+IF (a = 0) THEN a:= a * a * a; \
+IF (a < 0) THEN a: = a * a;\
+IF (b < 0) THEN b: = b * b;\
+IF (b > 0) THEN b: = b * b * b;\
+IF (c > 0) THEN c: = c * c * c;\
+IF (c < 0) THEN c: = c * c;\
+END. \
+"
 const lex = new lexer(program_text)
 
 let token_list = []
@@ -143,5 +262,5 @@ while (true) {
   token_list.push(lex.getNextToken())
   if (token_list[token_list.length - 1].type === "EOF") break
 }
-
-console.log(token_list)
+console.log(JSON.stringify(token_list, null, 4))
+console.log(lex.id_list, lex.constants_list, lex.reservedKeywords_list, lex.separators_list)
